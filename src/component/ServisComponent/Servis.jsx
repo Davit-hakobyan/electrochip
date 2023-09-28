3f683ddd005bac5341c5efc72da8b815ec0309dc
Davit-hakobyan <85398992+Davit-hakobyan@users.noreply.github.com>
2023-09-28 19:22:30 +0400

    electrochip
    v4

--- src/component/ServisComponent/Servis.jsx
+++ src/component/ServisComponent/Servis.jsx
@@ -61,7 +61,7 @@ function f1(){
             </div>
                <div  className='slid' >
                   <div  className='but1' style={{display:none?'block':'none'}} onClick={f} >
-                    <svg  id='svg' xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                    <svg  id='svgheder' xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
                       <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
                     </svg>
                   </div>
@@ -87,7 +87,7 @@ function f1(){
                     </div>
                   </div>
                   <div  onClick={f1}    className='but' style={{display:none1?'block':'none'}} >
-                    <svg  id='svg' xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                    <svg  id='svgheder' xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
                       <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
                     </svg>
                 </div>
